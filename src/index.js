87cf86dee31d292fe8f354083e9340d3cb89ec13
shenzekun <726664809@qq.com>
2017-09-14 22:48:54 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+    <h1>lello, world!</h1>,
+    document.getElementById('root')
+  );
