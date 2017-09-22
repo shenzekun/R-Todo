01290a4250ddc8b959b8e9fc0ea55d94039d43d6
shenzekun <726664809@qq.com>
2017-09-23 07:56:19 +0800

    增加错误弹出框

--- src/js/mod/ForgotPasswordForm.js
+++ src/js/mod/ForgotPasswordForm.js
@@ -7,16 +7,18 @@ export default class ForgotPasswordForm extends Component {
                 <h3>
                     重置密码
                 </h3>
-                <form className="forgotPassword" onSubmit={this.props.onSubmit}>
-                    <div className="row">
+
+                <form className="ui form" onSubmit={this.props.onSubmit} id="defaultForm">
+                    <div className="field">
                         <label>邮箱</label>
-                        <input type="text" value={this.props.formData.email}
-                               onChange={this.props.onChange.bind(null, 'email')}/>
+                        <div className="ui left icon input">
+                            <input type="text" placeholder="email" name="email" value={this.props.formData.email}
+                                   onChange={this.props.onChange.bind(null, 'email')}/>
+                            <i className="mail icon"></i>
+                        </div>
                     </div>
-                    <div className="row actions">
-                        <button type="submit">发送重置邮件</button>
-                        <a href="#" onClick={this.props.onSignIn}>返回登录</a>
-                    </div>
+                    <button className="ui fluid blue submit button" type="submit">发送重置邮件</button>
+                    <a href="#" onClick={this.props.onSignIn}>返回登录</a>
                 </form>
             </div>
         )
